46269ba4caf53f45edd73f36c09a8be313d0ff9f
Binesh Amarasekara <binesh.amarasekara@gmail.com>
2017-10-27 11:07:55 +0530

    Update and rename DeltaBot.java to DeltaBotAlpha.java

--- DeltaBotAlpha.java
+++ DeltaBotAlpha.java
@@ -1,7 +1,7 @@
 import robocode.*;
 import java.awt.geom.Point2D;
 
-public class DeltaBot extends AlphaBot {
+public class DeltaBotAlpha extends AlphaBot {
 	private EnemyBot enemy = new EnemyBot();
 	private byte radarDirection = 1;
 	private byte moveDirection = 1;
